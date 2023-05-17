cd516df042953eb652f9749d49efe7cca428031c
M. Ihza Deprian <ihza130502@gmail.com>
2023-05-17 17:35:35 +0700

    Sixteenth Commit

--- resources/views/midtrans/error.blade.php
+++ resources/views/midtrans/error.blade.php
@@ -4,10 +4,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="format-detection" content="telephone=no">
-        <title>Dzeko Laundry - Pembayaran Menggunakan Midtrans</title>
+        <title>Dzeko Laundry - Pembayaran Nontunai Menggunakan Midtrans</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <style>
+            @media screen and (max-width: 456px) {
+                .h1{
+                    font-size: 16px;
+                }
+            }
+        </style>
     </head>
     <body class="generic-template" style="-moz-osx-font-smoothing: grayscale; -webkit-font-smoothing: antialiased; background-color: #F5F5F5; margin: 0; padding: 0;">
         <!-- Header Start -->
@@ -16,12 +23,17 @@
                 <tr height="80">
                     <td align="left">
                         <a href="#">
+                            <img src="{{ asset('assets/images/image_midtrans_logo.png') }}" alt="DZEKO LAUNDRY" width="128" style="border: 0; font-size: 0; margin: 0; max-width: 100%; padding: 0;">
+                        </a>
+                    </td>
+                    <td align="left">
+                        <a href="#">
                             <img src="{{ asset('assets/images/image_wash_machine.png') }}" alt="DZEKO LAUNDRY" width="64" style="border: 0; font-size: 0; margin: 0; max-width: 100%; padding: 0;">
                         </a>
                     </td>
                     <td align="left"style="color: #000000; font-family: 'Poppins', sans-serif; font-size: 14px; line-height: 16px; vertical-align: middle;">
                         <a href="#" style="-webkit-text-decoration-color: #5A8DC3; color: #5A8DC3; text-decoration: none; text-decoration-color: #5A8DC3;">
-                            <h1>DZEKO LAUNDRY</h1>
+                            <h1 class="h1">DZEKO LAUNDRY</h1>
                         </a>        
                     </td>
                 </tr>
