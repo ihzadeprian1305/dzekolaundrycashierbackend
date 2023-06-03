a531e56826562cde3480bb680f4fed152b8fec20
M. Ihza Deprian <ihza130502@gmail.com>
2023-06-03 23:17:23 +0700

    twentieth sixth 5.0

--- app/Http/Controllers/Api/ExpenditureController.php
+++ app/Http/Controllers/Api/ExpenditureController.php
@@ -25,7 +25,7 @@ class ExpenditureController extends Controller
             $transaction = Expenditure::with('expenditure_items.stuffs', 'created_by', 'updated_by');
             
             if($request->search){
-                $transaction->whereUuid('id', '%'.$request->search.'%')->orWhere('information', 'like', '%'.$request->search.'%');
+                $transaction->where('information', 'like', '%'.$request->search.'%');
             }
 
             return response()->json([
